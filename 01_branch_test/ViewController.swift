b205823e72f781c7f245b9a173f7d633a8b367e4
D7703_28 <d7703_28@D7703-28ui-iMac.local>
2017-09-12 10:45:18 +0900

    add print develop branch

--- 01_branch_test/ViewController.swift
+++ 01_branch_test/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("1st commit")
+        print("2st commit")
+        print("I am in develop branch")
     }
 
     override func didReceiveMemoryWarning() {
